67c4d48a8644117d7365cf6d4a16b9066d682fc3
2Typpi <wrstle@web.de>
2021-02-24 10:38:54 +0100

    Change Table Name

--- models/user.js
+++ models/user.js
@@ -1,30 +1,35 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV1,
-            primaryKey: true
-        },
-        username: {type: DataTypes.STRING, allowNull: false},
-        hash: {type: DataTypes.STRING, allowNull: false},
-        firstName: {type: DataTypes.STRING, allowNull: false},
-        lastName: {type: DataTypes.STRING, allowNull: false},
-        email: {type: DataTypes.STRING, allowNull: false},
-        street: {type: DataTypes.STRING, allowNull: false},
-        houseNumber: {type: DataTypes.STRING, allowNull: false},
-        PLZ: {type: DataTypes.STRING, allowNull: false},
-        city: {type: DataTypes.STRING, allowNull: false}
-    }, {});
-    User.associate = (models) => {
-    };
+  const User = sequelize.define(
+    "users",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV1,
+        primaryKey: true,
+      },
+      username: { type: DataTypes.STRING, allowNull: false },
+      hash: { type: DataTypes.STRING, allowNull: false },
+      firstName: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false },
+      street: { type: DataTypes.STRING, allowNull: false },
+      houseNumber: { type: DataTypes.STRING, allowNull: false },
+      PLZ: { type: DataTypes.STRING, allowNull: false },
+      city: { type: DataTypes.STRING, allowNull: false },
+    },
+    {}
+  );
+  User.associate = (models) => {};
 
-    User.sync().then(function() {
-        console.log('User table successfully'); 
-    }, function(err) {
-        console.error('An error occurred while creating table : ' + err.stack);
-    });
+  User.sync().then(
+    function () {
+      console.log("User table successfully");
+    },
+    function (err) {
+      console.error("An error occurred while creating table : " + err.stack);
+    }
+  );
 
-
-    return User;
+  return User;
 };
